68311b46c2b460523eaf7bb877235f10ecdd76be
Brooke Hurford and Oskar Radon <brookebhurford@gmail.com>
2016-03-15 14:23:59 -0700

    Add .env file for API keys

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,11 +1,13 @@
-var apiKey = "018d3b7ce68b075d5e23307f49be7c2e";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $('#weatherLocation').click(function(){
     var city = $('#location').val();
     $("#location").val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error){
+      $('.showWeather').text(error.message);
     });
   });
 });
